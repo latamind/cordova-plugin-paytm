fd6d44b32acb3340923bf35e0ad361a73806ee5b
Sanjay Kumar Sahani <sanjaycedti@gmail.com>
2017-09-17 21:40:28 +0530

    unwarp module.exports

--- www/paytm.js
+++ www/paytm.js
@@ -1,10 +1,8 @@
-cordova.define("com.latamind.paytm.paytm", function (require, exports, module) {
-  module.exports = {
-    startPayment: function (merchantId, customerId, channelId, industryTypeId, website, orderId, amount, callbackUrl, checksum, isProd, successCallback, failureCallback) {
-      cordova.exec(successCallback,
-        failureCallback,
-        "Paytm",
-        "startPayment", [merchantId, customerId, channelId, industryTypeId, website, orderId, amount, callbackUrl, checksum, isProd]);
-    }
-  };
-});
+module.exports = {
+  startPayment: function (merchantId, customerId, channelId, industryTypeId, website, orderId, amount, callbackUrl, checksum, isProd, successCallback, failureCallback) {
+    cordova.exec(successCallback,
+      failureCallback,
+      "Paytm",
+      "startPayment", [merchantId, customerId, channelId, industryTypeId, website, orderId, amount, callbackUrl, checksum, isProd]);
+  }
+};
